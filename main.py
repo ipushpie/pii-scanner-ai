e46c4deaba9241fbb9936104bb0ad851e0f0902e
Pushpender Sharma <pushpendersharma2019@gmail.com>
2025-02-09 01:55:59 +0530

    wip

--- main.py
+++ main.py
@@ -4,11 +4,9 @@ from app.database import engine, Base  # Import the engine and Base
 
 app = FastAPI(
     title="PII Scanner API",
-    description="API for scanning documents for Personally Identifiable Information",
+    description="API for scanning documents for PIIs",
     version="1.0.0"
 )
-
-# Create the database tables if they do not exist
 Base.metadata.create_all(bind=engine)
 
 # Include routers
